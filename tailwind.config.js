99982fda35c4e23cfa013b1c61f5ee6b4cbe2c3f
Remzi Mustafaev <remarogue@gmail.com>
2024-04-28 14:57:12 +0300

    adding scroll animation to headings

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,14 +20,40 @@ module.exports = {
           '50%': { backgroundPosition: '100% 50%' },
           '100%': { backgroundPosition: '0% 50%' },
         },
+        animatedHeading: {
+          "0%": {
+            transform: "translateY(0%)",
+            opacity: '0'
+          },
+          "25%": {
+            opacity: '0.25'
+          },
+          "50%": {
+            transform: "translateY(-50%) scale(1.05)",
+            opacity: '0.5'
+          },
+          "75%": {
+            transform: "translateY(-25%) scale(1.15)",
+            opacity: '0.75'
+          },
+          "85%": {
+            opacity: '1'
+          },
+          "100%": {
+            transform: "translateY(0%) scale(1)",
+          },
+        },
       },
       backgroundSize: {
         '300%': '300%',
       },
       animation: {
         gradient: 'animatedgradient 9s ease infinite alternate',
+        animatedHeading: "animatedHeading linear forwards",
       },
-
+      supports: {
+        "no-scroll-driven-animations": "not(animation-timeline: scroll())",
+      },
     },
     screens: {
       'xs': '375px',
