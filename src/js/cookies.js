b68139c83b9964e5fb5bde56fab743d6e322f5b0
Remzi Mustafaev <remarogue@gmail.com>
2024-05-22 15:31:08 +0300

    added cookie handler

---
+++ src/js/cookies.js
@@ -0,0 +1,50 @@
+export function cookiesHandler() {
+  const eatlyCookie = "eatly-cookie";
+  const dialog = document.querySelector("#eatly-cookie");
+
+   function getCookie(name) {
+    if (window.cookieStore) {
+      return cookieStore.get(name);
+    } else {
+      const value = document.cookie
+        .split("; ")
+        .find((row) => row.startsWith(name))
+        ?.split("=")[1];
+      return value ? { value } : null;
+    }
+  }
+
+   function setCookie(name, value) {
+    const expiration = new Date();
+    expiration.setMonth(expiration.getMonth() + 6);
+
+    if (window.cookieStore) {
+      return cookieStore.set({
+        name: name,
+        value: value,
+        expires: expiration,
+      });
+    } else {
+      document.cookie = `${name}=${value}; expires=${expiration.toUTCString()}; samesite=strict; secure`;
+    }
+  }
+
+  getCookie(eatlyCookie).then((cookie) => {
+    if (cookie) {
+      if (cookie.value === "accept") {
+
+      }
+    } else {
+      dialog.show();
+
+      dialog.addEventListener("close", () => {
+        const value = dialog.returnValue;
+        setCookie(eatlyCookie, value);
+
+        if (value === "accept") {
+
+        }
+      });
+    }
+  });
+}
--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,13 @@
+import { cookiesHandler } from './cookies.js';
 import { themeToggle } from './theme-toggle.js';
 import { burger } from './burger.js';
 import { scrollTop } from './scroll-top.js';
 import { accordion } from './accordion.js';
 import { formValidate } from './formValidate.js';
 
-themeToggle()
-burger()
-scrollTop()
-accordion()
-formValidate()
+cookiesHandler();
+themeToggle();
+burger();
+scrollTop();
+accordion();
+formValidate();
